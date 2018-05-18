b8134a0c97b0a26d7ec3c9eb8de4c7819a7a4e07
Justin Rigler <justinmrigler@gmail.com>
2018-05-18 11:56:48 -0400

    completed user schema with bcrypt hash and compare functionality

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -8,13 +8,29 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 UserSchema.pre('save', function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
+  bcrypt.hash(this.password, 11, (err, hash) => {
+    // if there is an error here you'll need to handle it by calling next(err);
+    if (err) return next(err);
+
+    this.password = hash;
+    // Once the password is encrypted, call next() so that your userController and create a user
+    next();
+  });
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
@@ -22,6 +38,11 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(passwordGuess, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+
+    cb(null, isMatch);
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
